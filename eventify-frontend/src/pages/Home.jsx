ee0a18e8a565d14ec0c5153a2041adf066b7d36e
Ravindu Avi <se.ravindu@gmail.com>
2025-05-04 11:11:52 -0400

    feat: fe - app routing

--- eventify-frontend/src/pages/Home.jsx
+++ eventify-frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Typography } from "@mui/material";
+import EventList from "../components/EventList";
 
 function Home() {
   return (
     <div>
       <Typography variant="h4">Hi there! Welcome to Eventify 👋</Typography>
+      <EventList />
     </div>
   );
 }
